eb6ab29dcca5bb7f9b566f620fff209c039dc454
doubleu-gee <doubleu.gee10@gmail.com>
2017-12-27 15:35:43 +0400

    styled home page for mobiles

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,8 +4,8 @@ import React, { type Node } from 'react';
 
 import classes from './logo.scss';
 
-const Logo = (): Node => (
-  <div className={classes.logo}>
+const Logo = ({className} : {className: string}): Node => (
+  <div className={`${classes.logo} ${className}`}>
     <i className="fa fa-question-circle" />
     <span className={classes.logoText}>uizzo</span>
   </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,13 +7,13 @@ import classes from './home.scss';
 const Home = () => (
   <section className={classes.home}>
     <div className={classes.keyFeatures}>
-      <h1>A Trivia App That Feeds Your Brain</h1>
+      <h1 class={classes.heading}>A Trivia App That Feeds Your Brain</h1>
 
       <ul className={classes.featuresList}>
-        <li className={classes.featureItem}>Lorem Ipsum is simply dummy.</li>
-        <li className={classes.featureItem}>of the printing and typesetting.</li>
-        <li className={classes.featureItem}>industry. Lorem Ipsum has been .</li>
-        <li className={classes.featureItem}>the industry standard dummy</li>
+        <li className={classes.featureItem}><i className={`fa fa-check-circle ${classes.icon}`} aria-hidden="true"></i>Lorem Ipsum is simply dummy.</li>
+        <li className={classes.featureItem}><i className={`fa fa-check-circle ${classes.icon}`} aria-hidden="true"></i>industry. Lorem Ipsum has been .</li>
+        <li className={classes.featureItem}><i className={`fa fa-check-circle ${classes.icon}`} aria-hidden="true"></i>the industry standard dummy</li>
+        <li className={classes.featureItem}><i className={`fa fa-check-circle ${classes.icon}`} aria-hidden="true"></i>the industry standard dummy</li>
       </ul>
 
       <Button className={classes.desktopBtn}>take the quiz</Button>
@@ -22,7 +22,7 @@ const Home = () => (
     </div>
 
     <div className={classes.imageContainer}>
-      <img className={classes.image} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLcxgFJA2ON1mjdfK8eRgK5ISFdgLD-C9mNaUAa5U7AIouB4QK6w" alt="a girl reading a book" />
+      <img className={classes.image} src={'./girl.jpg'} />
       <Button className={classes.mobileBtn}>take the quiz</Button>
     </div>
   </section>
