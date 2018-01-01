3b92d84071d1696e9db68a77ce3c4227bb7fcfe4
doubleu-gee <doubleu.gee10@gmail.com>
2018-01-01 11:56:06 +0400

    styled <TextInput />

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -15,11 +15,11 @@ type State = {
 
 export default class TextInput extends React.Component<Props, State> {
     state = {
-      value: 'd',
+      value: '',
     }
 
-    handleChange(e: Event & { Target: HTMLInputElement } ) {
-      this.setState({ value: e.Target.value }, () => {
+    handleChange(e: Event & { target: HTMLInputElement } ) {
+      this.setState({ value: e.target.value }, () => {
         if (this.props.onChange) {
           this.props.onChange(this.props.id, this.state.value);
         }
@@ -32,7 +32,7 @@ export default class TextInput extends React.Component<Props, State> {
 
       return (
         <label className={classes.textInputWrapper} htmlFor={id}>
-          <span className={classes.label}>{label}</span>
+          <span className={classes.label}>{label}:</span>
           <input className={classes.textInput} type="text" id={id} value={this.state.value} onChange={e => this.handleChange(e)} />
         </label>
       );
