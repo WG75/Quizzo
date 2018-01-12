d53de2d514905f40d448481531f1ad1ee143bb2b
doubleu-gee <doubleu.gee10@gmail.com>
2018-01-12 10:09:11 +0400

    built <Form />

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -18,7 +18,7 @@ type State = {
 
 export default class Categories extends React.Component<Props, State> {
   state = {
-    selected: '',
+    selected: ''
   };
 
   handleChange(id: string, value: string) {
@@ -30,37 +30,42 @@ export default class Categories extends React.Component<Props, State> {
   }
 
   render() {
-    const { options, extraCategories } = this.props;
+    const { options, extraCategories, id } = this.props;
 
     return (
-      <div className={classes.categories}>
-        <div className={classes.categoriesWrapper}>
-          {options.map(category => (
-            <Category
-              key={category.value}
-              value={category.value}
-              icon={`fa-${category.icon}`}
-              onChange={() => this.handleChange(category.value, category.value)}
-              checked={
-                this.state.selected === category.value ||
-                (category.value === 'other' &&
-                  this.props.extraCategories.indexOf(this.state.selected) !==
-                    -1)
+      <div className={classes.fieldWrapper}>
+        <span className={classes.fieldLabel}>{id}</span>
+        <div className={classes.categories}>
+          <div className={classes.categoriesWrapper}>
+            {options.map(category => (
+              <Category
+                key={category.value}
+                value={category.value}
+                icon={`fa-${category.icon}`}
+                onChange={() =>
+                  this.handleChange(category.value, category.value)
+                }
+                checked={
+                  this.state.selected === category.value ||
+                  (category.value === 'other' &&
+                    this.props.extraCategories.indexOf(this.state.selected) !==
+                      -1)
+                }
+              />
+            ))}
+          </div>
+          {this.state.selected === 'other' ||
+          this.props.extraCategories.indexOf(this.state.selected) !== -1 ? (
+            <Select
+              className={classes.extraCategories}
+              options={extraCategories}
+              id="category"
+              onChange={(id: string, value: string) =>
+                this.handleChange(id, value)
               }
             />
-          ))}
+          ) : null}
         </div>
-        {this.state.selected === 'other' ||
-        this.props.extraCategories.indexOf(this.state.selected) !== -1 ? (
-          <Select
-            className={classes.extraCategories}
-            options={extraCategories}
-            id="category"
-            onChange={(id: string, value: string) =>
-              this.handleChange(id, value)
-            }
-          />
-        ) : null}
       </div>
     );
   }
--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -2,10 +2,12 @@
 
 import React from 'react';
 import TextInput from '../../components/TextInput/TextInput';
-import SelectMenu from '../../components/SelectMenu/SelectMenu';
+import RadioButtons from '../../components/RadioButtons/RadioButtons';
 import QuestionType from '../../components/QuestionType/QuestionType';
 import Categories from '../../components/Categories/Categories';
 import categoriesData from '../../data/categories';
+import Select from '../../components/SelectMenu/SelectMenu';
+import Button from '../../components/Button/Button';
 import classes from './form.scss';
 
 type Props = {};
@@ -14,10 +16,25 @@ type State = {};
 
 export default class Form extends React.Component<Props, State> {
   state = {};
-
+  
   render() {
     return (
-      <form />
+      <form>
+        <TextInput id="number" onChange={this.handleChange} label="number of questions"/>
+        <Categories id="catagory" onChange={this.handleChange} options={categoriesData} extraCategories={['extra1', 'extra2', 'extra3']}/>
+        
+        <div className={classes.fieldWrapper}>
+          <span className={classes.fieldLabel}>type:</span>
+          <RadioButtons className={classes.typeWrapper} id="type" onChange={this.handleChange} options={['true', 'multiple', 'any']} Label={QuestionType} labelClass={classes.labelClass} defaultChecked={0}/>
+        </div>
+        
+        <div className={classes.fieldWrapper}>
+          <span className={classes.fieldLabel}>diffculty:</span>
+          <Select className={classes.diffcultyWrapper} id="diffculty" onChange={this.handleChange} options={['easy', 'hard', 'medium']}  defaultSelected={0}/>
+        </div>
+
+        <Button>Start</Button>
+      </form>
     );
   }
 }
